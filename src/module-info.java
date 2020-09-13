73ef5b28df3d871a991929b5e5dd996b961f9ec4
Carlos Amengual <carlosame644+github@gmail.com>
2020-09-13 18:49:15 +0200

    module-info and POM <distributionManagement> was missing from initial import.

---
+++ src/module-info.java
@@ -0,0 +1,13 @@
+/*
+ *
+ * Copyright (c) 2005-2020, Carlos Amengual.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Licensed under a BSD-style License. You can find the license here:
+ * https://css4j.github.io/LICENSE.txt
+ *
+ */
+module io.sf.carte.tokenproducer {
+	exports io.sf.carte.uparser;
+}
